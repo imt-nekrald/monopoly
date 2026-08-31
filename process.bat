1cdf3c035ef2113332c5b004eaa52c3e61b222c6
Aliaksandr Nekrashevich <aliaksandr.nekrashevich@imt-atlantique.fr>
2026-08-31 18:13:22 +0200

    Updated process script.

--- process.bat
+++ process.bat
@@ -3,47 +3,49 @@ call devenv monopoly.sln /Build Release
 
 call conda activate
 
-call cd x64
-call cd Release
+cd x64
+cd Release
 
 if exist export-general\ (
-    call echo "Folder export-general already exists."
+    echo "Folder export-general already exists."
 ) else (
-    if exist evaluation-minimal.json (
-        call echo "File evaluation-minimal.json already exists."
+    if exist export-general\evaluation-minimal.json (
+        echo "File evaluation-minimal.json already exists."
     ) else ( 
-        call schedule_and_price.exe --instances minimal --approaches static rollout nested estimator --calls genetic gurobi  --report evaluation-minimal.json
+        call schedule_and_price.exe --instances minimal --approaches static rollout nested estimator --calls genetic gurobi  --report export-general/evaluation-minimal.json
+		move *.log logs/
+		move logs export-general\logs-minimal
     )
-    if exist evaluation-small.json (
-        call echo "File evaluation-small.json already exists."
+    if exist export-general\evaluation-small.json (
+        echo "File evaluation-small.json already exists."
     ) else (
-        call schedule_and_price.exe --instances small --approaches static rollout nested estimator --calls genetic gurobi  --report evaluation-small.json
+        call schedule_and_price.exe --instances small --approaches static rollout nested estimator --calls genetic gurobi  --report export-general/evaluation-small.json
+		move *.log logs/
+		move logs export-general\logs-small
     )
-    if exist evaluation-medium.json (
-        call echo "File evaluation-medium.json already exists."
+    if exist export-general\evaluation-medium.json (
+        echo "File evaluation-medium.json already exists."
     ) else (
-        call schedule_and_price.exe --instances medium --approaches static rollout nested estimator --calls genetic gurobi  --report evaluation-medium.json
+        call schedule_and_price.exe --instances medium --approaches static rollout nested estimator --calls genetic gurobi  --report export-general/evaluation-medium.json
+		move *.log logs/
+		move logs export-general\logs-medium
     )
-    call mkdir export-general
-    call move evaluation-minimal.json export-general\evaluation-minimal.json
-    call move evaluation-small.json export-general\evaluation-small.json
-    call move evaluation-medium.json export-general\evaluation-medium.json
 )
 
 if exist export-exact-one\ (
-    call echo "Folder export-exact-one already exists."
+    echo "Folder export-exact-one already exists."
 ) else (
     call fully_dynamic.exe
     call python ..\..\organize-one-exact.py --directory-root export-exact-one
 )
 
 if exist export-exact-two\ (
-    call echo "Folder export-exact-two already exists."
+    echo "Folder export-exact-two already exists."
 ) else (
     call dp-two-machines.exe
     call python ..\..\organize-two-exact.py --json-path results.json --export-dir export-exact-two
-    call move results.json export-exact-two/results.json
+    move results.json export-exact-two\results.json
 )
 
-call cd ..\..
-call echo "Results are availabe in x64/Release"
+cd ..\..
+echo "Results are available in x64\Release"
